ce9cfd4e439e31fec803bcf4452a4af2ff9264d5
suvindran3 <92168497+suvindran3@users.noreply.github.com>
2021-12-13 14:42:55 +0530

    Added the initial files

---
+++ index.js
@@ -0,0 +1,61 @@
+var wrongSound = new Audio('sounds/wrong.mp3');
+var gameOn = false;
+var currentState = 0;
+var colors = ['red', 'green', 'blue', 'yellow'];
+var gameState = [];
+
+
+if(screen.height > screen.width){
+  $('body').css('padding-top', '35vw');
+}
+
+function wrong() {
+  gameOn = false;
+  currentState = 0;
+  gameState = [];
+  $('body').css('background-color', 'red');
+  wrongSound.play();
+  setTimeout(function() {
+    $('body').css('background-color', '#011F3F');
+  }, 200);
+  $('h1').text('Game Over, Press a Key to Restart');
+}
+
+function nextState() {
+
+      gameState.push(colors[Math.floor(Math.random() * 4)]);
+      gameOn = true;
+      setTimeout(function(){
+          buttonClick(gameState[gameState.length - 1]);
+      }, 500);
+      $('h1').text('level ' + gameState.length );
+  
+}
+
+function buttonClick(button) {
+  $('.' + button).css('background-color', 'grey');
+  var audio = new Audio('sounds/' + button + '.mp3');
+  audio.play();
+  setTimeout(function() {
+    $('.' + button).css('background-color', button);
+  }, 200);
+}
+
+
+$('button').click(function(event) {
+  if (gameOn) {
+    buttonClick(event.target.className);
+    if (gameState[currentState] == event.target.className) {
+      if (currentState == gameState.length-1) {
+        nextState();
+        currentState = 0;
+      } else {
+        currentState++;
+      }
+    } else {
+      wrong();
+    }
+  }
+});
+
+$('body').keypress(nextState);
